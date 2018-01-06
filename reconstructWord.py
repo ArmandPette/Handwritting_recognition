e7e4beab2d8e4f3e5fff8c21540e474c8b09f762
PRuffy <pierre.ruffy@gmail.com>
2018-01-06 17:07:15 +0100

    Nouvelle méthode de reconstruction
    Rajout d'une méthode permettant de faire un test un peu plus pousser pour reconstruire le mot
    
    On commence par construire une matrice donnant, a partir du dictionnaire, les probabilité qu'une lettre soit suivit par une autre lettre.
    
    Ensuite si la meilleur probabilité d'une lettre est inférieur a un certain seuil on vas faire une vérification entre les deux meilleurs lettres renvoyé par le réseau et les valeur obtenu lors de l'analyse du dictionnaire

--- reconstructWord.py
+++ reconstructWord.py
@@ -1,16 +1,16 @@
 
+import numpy as np
+import re
 """
 On reçoit une liste d'array
 Chaque array possède la probabilité d'une lettre de l'alphabet (26 cases)
 """
 def wordConstruction(letterProbability):
     firstWordTry = dummyWordConstruction(letterProbability)
-    firstWordSize = len(firstWordTry)
     if(checkIfDummyWordIsValid(firstWordTry)):
         writtingWordInFiles(firstWordTry)
-
-
-
+    else :
+        cleverWordConstruction(letterProbability)
 
 def writtingWordInFiles(wordToAdd):
     with open("result.txt","a") as resultText :
@@ -18,6 +18,7 @@ def writtingWordInFiles(wordToAdd):
 
     resultText.close()
 
+"""Méthode Dummy"""
 
 def checkIfDummyWordIsValid (word):
     dummyPass = False
@@ -32,9 +33,9 @@ def checkIfDummyWordIsValid (word):
     words_Alpha.close()
     return dummyPass
 
-"""
-On essaye une méthode qui consiste simplement a prendre la lettre la plus probable pour chaque lettres
-"""
+
+#On essaye une méthode qui consiste simplement a prendre la lettre la plus probable pour chaque lettres
+
 def dummyWordConstruction (letterProbability):
     dummyWord = ""
     for letterArray in letterProbability :
@@ -49,6 +50,87 @@ def dummyWordConstruction (letterProbability):
 
 
     return dummyWord
+
+"""
+Vérification plus poussé
+A partir du dictionnaire l'objectif est de construire pour chaque lettre la probabilité d'être suivi par une autre lettre
+Lors de la reconstruction du mot si la probabilité la plus élèvé est inférieur a un seuil donnée
+    On compare alors les deux lettre les plus probable avec leur chance d'apparaitre
+    On garde alors le meilleursrésultat(par exemple multiplication des proba du réseau et celle obtenu sur le dictionnaire)
+"""
+
+def cleverWordConstruction(letterProbability):
+    filepathDictionnary = "words_alpha.txt"
+    #Valeur de seuil. Si proba en sortie du réseau de neuronne supérieur à cette valeur aucune vérification n'est faite
+    #Si la proba est inférieur alors on test avec la seconde lettres
+    SAFETY_VALUE = 0.7
+
+    #Variable contenant le tableau des probabilité des lettre en fonction du dictionnaire
+    count = constructStatFromDictionnary(filepathDictionnary)
+
+    returnWord = ""
+    firstLetter = True
+    for letterArray in letterProbability :
+        #Pour la première lettre on prend toujours la plus haute valeur disponible
+        if firstLetter :
+            max = 0
+            maxIndex = 0
+            for i in range(0, 25):
+                if letterArray[i] > max:
+                    max = letterArray[i]
+                    maxIndex = i
+
+            returnWord += addLetterToWord(maxIndex)
+
+            firstLetter = False
+
+        else :
+            max = 0
+            maxIndex = 0
+            secondMax = 0
+            secondMaxIndex = 0
+
+            for i in range(0,25):
+                if letterArray[i] > max :
+                    secondMax = max
+                    secondMaxIndex = maxIndex
+                    max = letterArray[i]
+                    maxIndex = i
+
+            if max > SAFETY_VALUE :
+                returnWord+= addLetterToWord(maxIndex)
+            else :
+                lastLetterIndex = ord(returnWord[-1:])-96
+                maxTest = max * count[lastLetterIndex-1,maxIndex]
+                secondMaxTest = secondMax * count[lastLetterIndex-1, secondMaxIndex]
+
+                if maxTest >= secondMaxTest :
+                    returnWord+=addLetterToWord(maxIndex)
+                else :
+                    returnWord+=addLetterToWord(secondMaxIndex)
+
+    writtingWordInFiles(returnWord)
+
+def constructStatFromDictionnary(filepath):
+    count = np.zeros(26,26)
+
+    with open(filepath,"r") as lines:
+        for line in lines:
+            #Pour chaque mot, a partir de la seconde lettre, regarder la lettre qui précède et ajouter dans count a la bonne case +1
+            line = line.replace("\n","")
+            line2 = list(line)
+            for i in range(1,len(line)):
+                firstLetter = ord(line2[i-1])-96 #-96 car en python ord('a') = 97
+                secondLetter = ord (line2[i])-96
+                #Une fois que les deux lettre sont identifié on incrémente de 1 dans la ligne de la première lettre
+                #colonne de la seconde lettre
+
+                count[firstLetter-1,secondLetter-1]+=1
+
+    #Pour être rigoureux il faudrait pour chaque ligne faire la somme des résultat obtenu et diviser chaque case par la valeur
+    #Mais pour l'utilisation que l'on veut faire par la suite ce n'est pas forcément nécessaire
+    lines.close()
+    return count
 
 def addLetterToWord(LetterNumber):
     return{
